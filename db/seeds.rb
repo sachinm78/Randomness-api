033762b8ff43e9268414767fd09dd6d9400e50a3
sachinm78 <sachinm78@gmail.com>
2019-08-07 20:26:32 -0500

    seeded db using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+    Superhero.create(name: Faker::Superhero.name, power: Faker::Superhero.power, prefix: Faker::Superhero.prefix, suffix: Faker::Superhero.suffix)
+end
