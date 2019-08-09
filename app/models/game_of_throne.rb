a320b49270e085889bfc0e0cf18e1cd2f65d0a4d
sachinm78 <sachinm78@gmail.com>
2019-08-09 15:24:50 -0500

    Revert "created game of thrones table and routes"
    This reverts commit 05651ec41c787fe4ff1b60aaaa1665dfcff64674.

--- app/models/game_of_throne.rb
+++
@@ -1,2 +0,0 @@
-class GameOfThrone < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :superheros, only: [:index, :show, :create]
   resources :starwars, only: [:index, :show, :create]
-  resources :game_of_thrones, only: [:index, :show, :create]
 
 end
